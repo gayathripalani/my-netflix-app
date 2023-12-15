b2e3c7c26d76d5b2d901acde215ad4fda3513e6d
Gayathri Palani <gayathri.palani7@gmail.com>
2023-12-15 13:49:22 +0100

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -22,7 +22,6 @@ export const API_OPTIONS = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNjFlZTk0ODk0ZTc0NzkzN2M1YWRkNmQ3NGQ1YTNmYyIsInN1YiI6IjY1NmNmMWEwNGE0YmY2MDEwMzUwODk1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bhjoygFg95wW7aSZqQ3oHuWRebBxAF9gQDrtbxxtHVU",
     },
   };
   
